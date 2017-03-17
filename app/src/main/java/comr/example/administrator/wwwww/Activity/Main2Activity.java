974715880161f7af5b89dc52cecc49ab6afb09be
jiang <406176962@qq.com>
2017-03-17 11:04:49 +0800

    new

---
+++ app/src/main/java/comr/example/administrator/wwwww/Activity/Main2Activity.java
@@ -0,0 +1,89 @@
+package comr.example.administrator.wwwww.Activity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import comr.example.administrator.wwwww.R;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+    private NavigationView navigationView;
+    private Button b1;
+    private Button b2;
+    private Button b3;
+    private Button b4;
+    private Button b5;
+    private Button b6;
+    private Button b7;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        navigationView = (NavigationView) findViewById(R.id.main2_nv);
+        getChe();
+    }
+
+    private void getChe() {
+        //设置侧边栏的布局
+        navigationView.inflateHeaderView(R.layout.ce_lauot);
+
+        //拿到侧边栏的整个的View ，可以找到空间，通常是用 0 。
+        View view = navigationView.getHeaderView(0);
+
+        //拿到侧边栏的一个ImageView 设置他的图片
+        SimpleDraweeView draweeView = (SimpleDraweeView) view.findViewById(R.id.ce_back);
+        SimpleDraweeView phtot = (SimpleDraweeView) view.findViewById(R.id.ce_photo);
+        draweeView.setImageURI(Uri.parse("http://pic.51yuansu.com/pic2/cover/00/16/21/579ae4930e4c1_610.jpg"));
+        phtot.setImageURI(Uri.parse("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1489726572893&di=0cddbca12088723924109669bb3e4fd6&imgtype=0&src=http%3A%2F%2Fimg3.duitang.com%2Fuploads%2Fitem%2F201510%2F05%2F20151005045731_zNKAS.thumb.224_0.jpeg"));
+        b1 = (Button) view.findViewById(R.id.ce_b1);
+        b2 = (Button) view.findViewById(R.id.ce_b2);
+        b3 = (Button) view.findViewById(R.id.ce_b3);
+        b4 = (Button) view.findViewById(R.id.ce_b4);
+        b5 = (Button) view.findViewById(R.id.ce_b5);
+        b6 = (Button) view.findViewById(R.id.ce_b6);
+        b7 = (Button) view.findViewById(R.id.ce_b7);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ce_b1:
+                Toast.makeText(Main2Activity.this, "点击", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ce_b2:
+                Toast.makeText(Main2Activity.this, "点击", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ce_b3:
+                Toast.makeText(Main2Activity.this, "点击", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ce_b4:
+                Toast.makeText(Main2Activity.this, "点击", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ce_b5:
+                Toast.makeText(Main2Activity.this, "点击", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ce_b6:
+                Toast.makeText(Main2Activity.this, "点击", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ce_b7:
+                Toast.makeText(Main2Activity.this, "点击", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
--- app/src/main/java/comr/example/administrator/wwwww/Activity/MainActivity.java
+++ app/src/main/java/comr/example/administrator/wwwww/Activity/MainActivity.java
@@ -2,6 +2,9 @@ package comr.example.administrator.wwwww.Activity;
 
 import android.net.Uri;
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,8 +18,12 @@ import comr.example.administrator.wwwww.R;
  * Created by Administrator on 2017/3/17 0017.
  */
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity  {
     private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private Toolbar toolbar;
+//    private ImageButton imageButton;
     private Button b1;
     private Button b2;
     private Button b3;
@@ -32,7 +39,14 @@ public class MainActivity extends BaseActivity {
     @Override
     public void initView() {
         navigationView=(NavigationView)findViewById(R.id.main_nv);
+//        imageButton=(ImageButton)findViewById(R.id.mian_ce) ;
+//        imageButton.setOnClickListener(this);
+        drawerLayout=(DrawerLayout)findViewById(R.id.main_dl);
+        toolbar=(Toolbar)findViewById(R.id.main_tb);
+        toolbar.setTitle("首页");
+
         getChe();
+        initDrawer(toolbar);
     }
 
     @Override
@@ -70,6 +84,9 @@ public class MainActivity extends BaseActivity {
             case R.id.ce_b7:
                 Toast.makeText(MainActivity.this,"点击",Toast.LENGTH_SHORT).show();
                 break;
+//            case R.id.mian_ce:
+//                drawerLayout.openDrawer(GravityCompat.START);
+//                break;
         }
     }
     private void getChe(){
@@ -83,7 +100,7 @@ public class MainActivity extends BaseActivity {
         SimpleDraweeView draweeView = (SimpleDraweeView) view.findViewById(R.id.ce_back);
         SimpleDraweeView phtot=(SimpleDraweeView)view.findViewById(R.id.ce_photo);
         draweeView.setImageURI(Uri.parse("http://pic.51yuansu.com/pic2/cover/00/16/21/579ae4930e4c1_610.jpg"));
-        phtot.setImageURI(Uri.parse("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1489726572893&di=0cddbca12088723924109669bb3e4fd6&imgtype=0&src=http%3A%2F%2Fimg3.duitang.com%2Fuploads%2Fitem%2F201510%2F05%2F20151005045731_zNKAS.thumb.224_0.jpeg"));
+        phtot.setImageURI(Uri.parse("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1489729877495&di=f5cf9bdcda77e5aaa099a4d4592daa3a&imgtype=0&src=http%3A%2F%2Fscimg.jb51.net%2Fallimg%2F160714%2F102-160G4220036118.jpg"));
         b1=(Button)view.findViewById(R.id.ce_b1);
         b2=(Button)view.findViewById(R.id.ce_b2);
         b3=(Button)view.findViewById(R.id.ce_b3);
@@ -100,5 +117,23 @@ public class MainActivity extends BaseActivity {
         b7.setOnClickListener(this);
 
 
+    }
+
+    public void initDrawer(Toolbar toolbar) {
+        if (toolbar != null) {
+            mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close) {
+                @Override
+                public void onDrawerOpened(View drawerView) {
+                    super.onDrawerOpened(drawerView);
+                }
+
+                @Override
+                public void onDrawerClosed(View drawerView) {
+                    super.onDrawerClosed(drawerView);
+                }
+            };
+            mDrawerToggle.syncState();
+            drawerLayout.addDrawerListener(mDrawerToggle);
+        }
     }
 }
